ffff2fc9cb74540b7413a951faa10335ad4f0863
ManavLohabade <manavlohabade26@gmail.com>
2025-01-09 22:25:21 +0530

    Addded Que 2185

---
+++ Que2185.cpp
@@ -0,0 +1,15 @@
+// Counting words with a given prefix
+
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int ans = 0;
+        for (auto s : words) {
+            auto found = s.find(pref);
+            if (found != string ::npos && found == 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
