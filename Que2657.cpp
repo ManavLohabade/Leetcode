f232cd7476cfb78b08c4a73b07d69be3ae8755b3
ManavLohabade <manavlohabade26@gmail.com>
2025-01-14 23:52:43 +0530

    Added Que 2657

---
+++ Que2657.cpp
@@ -0,0 +1,16 @@
+// Find the Prefix Common of two strings
+
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        int n = A.size();
+        long long a = 0, b = 0, one = 1;
+        vector<int> ans(n);
+        for (int i = 0; i < n; i++) {
+            a |= one << A[i];
+            b |= one << B[i];
+            ans[i] = __builtin_popcountll(a & b); // Count set bits
+        }
+        return ans;
+    }
+};
