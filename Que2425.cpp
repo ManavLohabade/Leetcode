6be1d09fbb22f67b42621510dc5d7e667bbc4fdb
ManavLohabade <manavlohabade26@gmail.com>
2025-01-16 23:47:49 +0530

    Added Que 2425

---
+++ Que2425.cpp
@@ -0,0 +1,26 @@
+// Bitwsie XOR of all Pairsings
+
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size(), m = nums2.size();
+        int ans = 0;
+        if (n % 2 == 1 && m % 2 == 1) {
+            for (int i = 0; i < m; i++) {
+                ans ^= nums2[i];
+            }
+            for (int i = 0; i < n; i++) {
+                ans ^= nums1[i];
+            }
+        } else if (n % 2 == 1 && m % 2 == 0) {
+            for (int i = 0; i < m; i++) {
+                ans ^= nums2[i];
+            }
+        } else if (n % 2 == 0 && m % 2 == 1) {
+            for (int i = 0; i < n; i++) {
+                ans ^= nums1[i];
+            }
+        }
+        return ans;
+    }
+};
