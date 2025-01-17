73ece0d4c5313371721be971242740a1c4e20f53
ManavLohabade <manavlohabade26@gmail.com>
2025-01-17 18:46:04 +0530

    Added Que 2683

---
+++ Que2683.cpp
@@ -0,0 +1,15 @@
+//Neighboring
+
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) {
+        int x=0;
+
+        for(auto it:derived)
+        {
+            x=x^it;
+        }
+
+        return x==0;
+    }
+};
