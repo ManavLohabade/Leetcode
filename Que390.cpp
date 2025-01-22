cb41153bf859633cbe546f661b8e9006c08c98a3
ManavLohabade <manavlohabade26@gmail.com>
2025-01-22 23:06:19 +0530

    Added Que 390

---
+++ Que390.cpp
@@ -0,0 +1,23 @@
+// Elimination Game 
+
+class Solution {
+public:
+    int lastRemaining(int n) {
+        bool left_to_right = true;
+        int remaining = n;
+        int step = 1;
+        int head = 1;
+
+        while (remaining > 1) {
+            if (left_to_right || remaining % 2 == 1) {
+                head += step;
+            }
+
+            remaining /= 2;
+            step *= 2;
+            left_to_right = !left_to_right;
+        }
+
+        return head;
+    }
+};
