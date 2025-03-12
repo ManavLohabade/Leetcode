0ecbd31362575c14fefebe415ee6ae68409a27ae
ManavLohabade <manavlohabade26@gmail.com>
2025-03-13 00:02:07 +0530

    Added Que 2529

---
+++ Que2529.cpp
@@ -0,0 +1,49 @@
+// 
+Testcase
+Test Result
+Test Result
+Code
+Code
+Binary Search Approach - Finding First Non-Negative and First Positive Index
+2529. Maximum Count of Positive Integer and Negative Integer
+
+class Solution {
+    public:
+        int maximumCount(vector<int>& nums) {
+            int n = nums.size();
+            int l=0, r=n;
+    
+            int firstNonNeg = n;
+    
+            // first zero or positive number index 
+            while(l<r) {
+              int mid = l+(r-l)/2;
+              if(nums[mid]>=0) {
+                r = mid;
+                firstNonNeg = mid;
+                continue;
+              } else {
+                l = mid + 1;
+              }
+            }
+    
+            int firstPos = n;
+    
+            l = 0;
+            r = n;
+            // first positive number index 
+            while(l<r) {
+              int mid = l+(r-l)/2;
+              if(nums[mid]>0) {
+                r = mid;
+                firstPos = mid;
+                continue;
+              } else {
+                l = mid + 1;
+              }
+            }
+    
+            return max(n-firstPos, firstNonNeg);
+            
+        }
+    };
