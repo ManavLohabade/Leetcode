4d72474d07a42f748adcbda959771032b3efa457
ManavLohabade <manavlohabade26@gmail.com>
2025-03-19 23:31:43 +0530

    Added Que 3191

--- Que2401.cpp
+++ Que2401.cpp
@@ -1,2 +1,21 @@
 // Longest Nice Subarray 
 
+class Solution {
+    public:
+        int longestNiceSubarray(vector<int>& nums) {
+            int result = 1;
+            int n = nums.size();
+            int l = 0, r = 0; 
+            int bitMask = 0;
+            while(r < n) {
+              while((bitMask & nums[r]) != 0) {
+                bitMask ^= nums[l];
+                l++;
+              }
+              bitMask |= nums[r];
+              result = max(r-l+1, result);
+              r++;
+            }
+            return result;
+        }
+    };
---
+++ Que3191.cpp
@@ -0,0 +1,18 @@
+// Minimum Operations to make binary Array Elements Equal to one I
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n = nums.size(),ans = 0;
+        for(int i = 0;i < n;i ++){
+            if(nums[i] == 0){
+                ans ++;
+                if(i + 1 >= n || i + 2 >= n) return -1;
+                nums[i + 1] = 1 - nums[i + 1];
+                nums[i + 2] = 1 - nums[i + 2];
+            }
+        }
+
+        return ans;
+    }
+};
