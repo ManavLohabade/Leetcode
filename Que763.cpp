8f9d8c2481541e5897001c64c417743e11260141
ManavLohabade <manavlohabade26@gmail.com>
2025-03-30 23:54:12 +0530

    Added Que 2818 & 763

---
+++ Que763.cpp
@@ -0,0 +1,30 @@
+// Partitions Labels
+
+class Solution {
+    public:
+        vector<int> partitionLabels(string s) {
+            int n=s.size();
+            unordered_map<char,int>mp;
+            for(int i=0;i<n;i++)
+            {
+                mp[s[i]]=i;
+            }
+            int size=0;
+            int end=0;
+            vector<int>ans;
+            for(int i=0;i<n;i++)
+            {
+                if(mp[s[i]]>end)
+                {
+                    end=mp[s[i]];
+                }
+                size++;
+                if(i==end)
+                {
+                    ans.push_back(size);
+                    size=0;
+                }
+            }
+            return ans;
+        }
+    };
